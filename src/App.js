a6cb84c791c7432947925208407c428e01c29647
gabecpf97 <gabrielcpf2997@gmail.com>
2022-01-12 00:22:50 -0800

    first commit basic file created and can play video now

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video width="500px">
+        <source src='http://localhost:5001/media/?name=652333414.mp4' 
+          crossOrigin='' type="video/mp4"/>
+      </video>
     </div>
   );
 }
