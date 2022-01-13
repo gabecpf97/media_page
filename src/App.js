12f49e47c73a790fde56e4ba54d7fa5396e02e53
Gabriel <gabrielcpf2997@gmail.com>
2022-01-12 18:45:24 -0800

    added input to find file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [filename, setFileName] = useState();
+  const [url, setUrl] = useState();
+
+  const handleNameChange = (e) => {
+    setFileName(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(filename);
+  }
+
   return (
     <div className="App">
-      <video width="500px">
-        <source src='http://localhost:5001/media/?name=652333414.mp4' 
-          crossOrigin='' type="video/mp4"/>
-      </video>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" onChange={(e) => handleNameChange(e)} required={true} />
+        <input type="submit" value="find"/>
+      </form>
+      {url && 
+        <video width="500px">
+          <source src={`http://localhost:5001/media/?name=${url}`} 
+            crossOrigin='' type="video/mp4"/>
+        </video>
+      }
     </div>
   );
 }
